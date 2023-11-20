2b95068122ac75bedcc2172a20ecd038aa2cb508
Carlos Daniel <carlos.daniel.0486@gmail.com>
2023-11-20 18:39:15 -0300

    Ajustes nos caminhos das imagens

--- src/data/CardsData.tsx
+++ src/data/CardsData.tsx
@@ -1,42 +1,50 @@
+import miami from '../../public/images/miami.png'
+import japan from '../../public/images/japan.png'
+import dubai from '../../public/images/dubai.png'
+import tai from '../../public/images/tai.png'
+import newO from '../../public/images/new.png'
+import greee from '../../public/images/gree.png'
+
+
 const CardsData = [
   {
     title: 'Miami Beaches',
-    image: 'public/images/miami.png',
+    image: miami,
     model: 'Chill',
     value: 850,
     time: '200',
   },
   {
     title: 'Japan',
-    image: '/images/japan.png',
+    image: japan,
     model: 'Relax',
     value: 1200,
     time: '400',
   },
   {
     title: 'Dubai',
-    image: '/images/dubai.png',
+    image: dubai,
     model: 'Chill',
     value: 500,
     time: '600',
   },
   {
     title: 'Taj-Mahal',
-    image: '/images/tai.png',
+    image: tai,
     model: 'Relax',
     value: 350,
     time: '200',
   },
   {
     title: 'New York',
-    image: '/images/new.png',
+    image: newO,
     model: 'Chill',
     value: 500,
     time: '400',
   },
   {
     title: 'Greece',
-    image: '/images/gree.png',
+    image: greee,
     model: 'Relax',
     value: 950,
     time: '600',
